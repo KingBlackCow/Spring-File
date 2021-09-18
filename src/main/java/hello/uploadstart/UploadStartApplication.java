21efa44208b247f22a5fe3530014fdb62aeade48
KingBlackCow <sgs1159@naver.com>
2021-09-18 10:55:21 +0900

    first-init

---
+++ src/main/java/hello/uploadstart/UploadStartApplication.java
@@ -0,0 +1,13 @@
+package hello.uploadstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadStartApplication.class, args);
+	}
+
+}
